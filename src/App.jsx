40cf66299ee6efec4e92fd169ca4c38507154e3d
Matija Galina <matijagalina@MacBook-Pro-od-Matija.local>
2023-11-09 12:05:55 +0100

    initial

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [licenseData, setLicenseData] = useState(null);
+
+  useEffect(() => {
+    // Fetch license information from the API
+    fetch('/api/v1/license')
+      .then((response) => response.json())
+      .then((data) => setLicenseData(data))
+      .catch((error) => console.error('Error fetching license:', error));
+  }, []);
+
+  return (
+    <div>
+      <h2>License Information</h2>
+      {licenseData ? (
+        <pre>{JSON.stringify(licenseData, null, 2)}</pre>
+      ) : (
+        <p>Loading license information...</p>
+      )}
+    </div>
+  );
+}
+
+export default App
